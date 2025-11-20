d2b2e08679369622e006eea6734f31d89da7a5e4
gwonsujin <gwonsujin16@gmail.com>
2025-11-20 13:38:36 +0900

    🗃️MongoDB 연결

--- w25w11-mongodb-backend/build.gradle.kts
+++ w25w11-mongodb-backend/build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
